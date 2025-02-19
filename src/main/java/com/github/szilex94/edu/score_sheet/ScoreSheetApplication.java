9a7365d81683ce0c5631fa8cf4bdd1641330fea6
Mate Szilard <32964548+Szilex94@users.noreply.github.com>
2025-02-19 23:16:34 +0200

    Initial commit

---
+++ src/main/java/com/github/szilex94/edu/score_sheet/ScoreSheetApplication.java
@@ -0,0 +1,13 @@
+package com.github.szilex94.edu.score_sheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/szilex94/edu/score_sheet/ScoreSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szilex94.edu.score_sheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
